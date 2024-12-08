36810c77d1813d1bbc72401c05c9f8402278dd04
Nitin J <94215235+Nitin1234567898@users.noreply.github.com>
2024-12-08 19:52:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const noButton = document.getElementById('no');
+const yesButton = document.getElementById('yes');
+const mainContent = document.getElementById('main-content');
+const finalContent = document.getElementById('final-content');
+
+
+function move(){
+    const x = Math.random() * (window.innerWidth - noButton.offsetWidth);
+    const y = Math.random() * (window.innerHeight - noButton.offsetHeight);
+    noButton.style.position = 'absolute';
+    noButton.style.left = `${x}px`;
+    noButton.style.top = `${y}px`;
+};
+noButton.addEventListener('mouseover', move); // For desktops
+noButton.addEventListener('touchstart', (event) => {
+    event.preventDefault(); 
+    setTimeout(moveNoButton, 100); 
+});
+
+yesButton.addEventListener('click', () => {
+    mainContent.style.display = 'none'; 
+    finalContent.style.display = 'block'; /
+});
